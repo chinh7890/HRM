ff9e1eaa9d979ce8c3be5e3ec95b2949002cbb62
dynn3010 <linhoppa7@gmail.com>
2023-08-17 16:29:30 +0700

    hienthilist00000

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
-   $conn = new mysqli("localhost","root","new_password","HRM");
+   $conn = new mysqli("localhost","root","","qlns");
 
    // Kiểm tra kết nối
     if ($conn->connect_error) {
         die("Kết nối thất bại: " . $conn->connect_error);
     }
     echo "Kết nối thành công";
-    $conn->close();
+?>
