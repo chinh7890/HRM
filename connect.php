899b6fd2e416ed134264d21e14327794862f6fdd
dynn3010 <linhoppa7@gmail.com>
2023-08-29 16:23:58 +0700

    4:08-29/08-linh-upfile-lan3

--- connect.php
+++ connect.php
@@ -1,17 +1,10 @@
 <?php
-<<<<<<< HEAD
-//    $conn = new mysqli("localhost","root","new_password","HRM");
 
-   $conn = new mysqli("localhost","root","","qlns");
-//    $conn = new mysqli("localhost","root","","qlns");
-   
-=======
-$conn = new mysqli("localhost", "root", "new_password", "HRM");
+// $conn = new mysqli("localhost", "root", "new_password", "HRM");
 //    $conn = new mysqli("localhost","root","","hrm_database");
-//    $conn = new mysqli("localhost","root","","qlns");
+   $conn = new mysqli("localhost","root","","qlns");
 // $conn = mysqli_connect("sql211.byethost7.com", "b7_34919410", "Chinhnguyen321", "b7_34919410_hrm");
 
->>>>>>> c971035a78a8a1089d9b94c22e3ce3ac95374db0
 // Kiểm tra kết nối
 if ($conn->connect_error) {
     die("Kết nối thất vọng: " . $conn->connect_error);
