6efca58a0acae3ba2812e077ec4e7c9db3a3b206
Your Name <chinhnguyen6930@gmail.com>
2023-08-29 14:28:21 +0700

    2:28 2908

--- connect.php
+++ connect.php
@@ -1,11 +1,12 @@
 <?php
-   $conn = new mysqli("localhost","root","new_password","HRM");
+$conn = new mysqli("localhost", "root", "new_password", "HRM");
 
 //    $conn = new mysqli("localhost","root","","qlns");
 //    $conn = new mysqli("localhost","root","","qlns");
-   
+// $conn = mysqli_connect("sql211.byethost7.com", "b7_34919410", "Chinhnguyen321", "b7_34919410_hrm");
 // Kiểm tra kết nối
-    if ($conn->connect_error) {
-        die("Kết nối thất vọng: " . $conn->connect_error);
-    }
+if ($conn->connect_error) {
+    die("Kết nối thất vọng: " . $conn->connect_error);
+}
+mysqli_set_charset($conn, "utf8");
     // echo "Kết nối thành công";
