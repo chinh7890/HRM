c88fdec865662ae4572ca2fc30c2785536a26221
TranLam848 <trhlam.c3pq2020@gmail.com>
2023-08-29 14:38:36 +0700

    14382908

--- connect.php
+++ connect.php
@@ -2,6 +2,8 @@
 //    $conn = new mysqli("localhost","root","new_password","HRM");
    $conn = new mysqli("localhost","root","","hrm_database");
 //    $conn = new mysqli("localhost","root","","qlns");
+// $conn = mysqli_connect("sql211.byethost7.com", "b7_34919410", "Chinhnguyen321", "b7_34919410_hrm");
+
 // Kiểm tra kết nối
 if ($conn->connect_error) {
     die("Kết nối thất vọng: " . $conn->connect_error);
